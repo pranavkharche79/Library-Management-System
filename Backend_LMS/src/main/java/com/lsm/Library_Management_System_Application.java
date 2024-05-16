904c1f0382773287eca24a22b9813f868e75aacf
pranavkharche79 <pranavkharche79@gmail.com>
2024-05-16 21:49:29 +0530

    Backend Architecture is ready

---
+++ Backend_LMS/src/main/java/com/lsm/Library_Management_System_Application.java
@@ -0,0 +1,12 @@
+package com.lsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Library_Management_System_Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Library_Management_System_Application.class, args);
+	}
+}
