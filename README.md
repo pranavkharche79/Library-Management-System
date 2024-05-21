de085854a3056a1a2ff41ed421b1e8dba6c05a32
pranavkharche79 <pranavkharche79@gmail.com>
2024-05-21 13:48:03 +0530

    Some working Dev images

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# Library Management System
+# Library Management System ( InnoByte Services )
+<br/>
+
+## Home Page
+<img src="/Images/0.png"/>
+<br/>
+
+## Admin
+
+### Login
+<img src="/Images/Admin/1.png"/>
+<br/>
+
+### Admin Dashboard
+<img src="/Images/Admin/2.png"/>
+<br/>
+
+### Add Book
+<img src="/Images/Admin/3.png"/>
+<br/>
+
+### Book Search
+<img src="/Images/Admin/4.png"/><br/>
+<img src="/Images/Admin/4_1.png"/><br/>
+<img src="/Images/Admin/4_2.png"/>
+<br/>
+
+### Book Update
+<img src="/Images/Admin/5.png"/><br/>
+<img src="/Images/Admin/5_1.png"/><br/>
+<img src="/Images/Admin/5_2.png"/><br/>
+<img src="/Images/Admin/5_3.png"/>
+<br/>
+
+### View Order
+<img src="/Images/Admin/6.png"/><br/>
+<img src="/Images/Admin/6_1.png"/><br/>
+<img src="/Images/Admin/6_2.png"/>
+<br/>
+
+### Signout
+<img src="/Images/Admin/7.png"/>
+<br/>
+<br/>
+
+## Student
+
+### Login
+<img src="/Images/Student/1.png"/>
+<br/>
+
+
+### Book Search
+<img src="/Images/Student/2.png"/><br/>
+<img src="/Images/Student/2_1.png"/><br/>
+<img src="/Images/Student/2_2.png"/>
+<br/>
+
+### View Data Info
+<img src="/Images/Student/3.png"/><br/>
+<img src="/Images/Student/3_1.png"/><br/>
+<img src="/Images/Student/3_2.png"/>
+<br/>
+
+### Return Book
+<img src="/Images/Student/4.png"/>
+<br/>
+
+### Place Order
+<img src="/Images/Student/5.png"/>
+<br/>
+
+### Logout
+<img src="/Images/Student/6.png"/>
+<br/>
+<br/>
+
+#Name :- Pranav Kharche
---
+++ lsm.sql
@@ -0,0 +1,135 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: library_management_system
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admins` (
+  `adminid` bigint NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`adminid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admins`
+--
+
+LOCK TABLES `admins` WRITE;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+INSERT INTO `admins` VALUES (1,'Admin','Admin');
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `cost` double DEFAULT NULL,
+  `quantity` int NOT NULL,
+  `bookid` bigint DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `author` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_7lvlaxo8n08yf1oiam4om0t2l` (`bookid`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (16,20,130,1,'D.H.Lawerence','The Rainbow'),(30,10,131,2,'Arthar Chrisro','Darkness At Noon'),(22,10,132,3,'Ralwath Edison','Invisible Man'),(30,44,133,4,'Joshep Conway','Secret Agent'),(25,10,134,5,'Edward James','The Age Of Innocence'),(250,15,135,6,'S. Radhakrishnan','Bhagavad Gita');
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `borrowedbooks`
+--
+
+DROP TABLE IF EXISTS `borrowedbooks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `borrowedbooks` (
+  `expiry_date` varchar(255) DEFAULT NULL,
+  `issue_date` varchar(255) DEFAULT NULL,
+  `return_date` varchar(255) DEFAULT NULL,
+  `book_id` bigint DEFAULT NULL,
+  `borrowid` bigint NOT NULL AUTO_INCREMENT,
+  `student_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`borrowid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `borrowedbooks`
+--
+
+LOCK TABLES `borrowedbooks` WRITE;
+/*!40000 ALTER TABLE `borrowedbooks` DISABLE KEYS */;
+INSERT INTO `borrowedbooks` VALUES ('20/06/2024','20/05/2024',NULL,133,1,101);
+/*!40000 ALTER TABLE `borrowedbooks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `sfine` bigint DEFAULT NULL,
+  `sid` bigint NOT NULL,
+  `sname` varchar(255) DEFAULT NULL,
+  `spass` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (0,101,'John','John@123'),(0,102,'Pranav','Pranav@123');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-21  0:17:22
